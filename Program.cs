97004013eb620d194b9044a6a97425d58e70782d
Isak <Isak>
2024-01-29 14:05:32 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.Run();
+
+string AddNumbers(int num1, int num2)
+{
+    return($"sum of {num1} and {num2} is {num1 + num2}");
+}
