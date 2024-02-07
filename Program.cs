d5b1ba570cc1794cfd8622a7e4b7990ac2bc9331
Isak <Isak>
2024-02-07 14:28:08 +0100

    Final

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using application;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,29 +9,34 @@ var app = builder.Build();
 
 app.MapGet("/", () => "/encrypt for encryption and /decrypt for decrypt\nSyntax: /[endpoint]?input='[string]'&key=[int]\n[IMPORTANT]--> Key must be larger than 0 and smaller than 27");
 
-app.MapGet("/encrypt", (string input, int key) => Encrypt(input, key));
+app.MapGet("/encrypt", (string input, int key) => Crypt.Encrypt(input, key));
 
-app.MapGet("/decrypt", (string input, int key) => Decrypt(input, key));
+app.MapGet("/decrypt", (string input, int key) => Crypt.Decrypt(input, key));
 
 app.Run();
 
-
-static string Encrypt(string input, int key)
+namespace application
 {
-    char[] result = input.ToCharArray();
-
-    for (int i = 0; i < result.Length; i++)
+    class Crypt
     {
-        if (char.IsLetter(result[i]))
+        public static string Encrypt(string input, int key)
         {
-            char offset = char.IsUpper(result[i]) ? 'A' : 'a';
-            result[i] = (char)(((result[i] + key - offset) % 26) + offset);
+            char[] result = input.ToCharArray();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (char.IsLetter(result[i]))
+                {
+                    char offset = char.IsUpper(result[i]) ? 'A' : 'a';
+                    result[i] = (char)(((result[i] + key - offset) % 26) + offset);
+                }
+            }
+            return new string(result);
+        }
+
+        public static string Decrypt(string input, int key)
+        {
+            return Encrypt(input, 26 - key);
         }
     }
-    return new string(result);
-}
-
-static string Decrypt(string input, int key)
-{
-    return Encrypt(input, 26 - key);
 }
